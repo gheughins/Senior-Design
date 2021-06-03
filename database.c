f36abd97992fe03daf48ca791ccb42c816131368
Gabby Heughins <heughinsg@gmail.com>
2021-06-03 17:33:10 -0400

    Update

--- database.c
+++ database.c
@@ -3,23 +3,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#define SIZE 100;
+
 struct product {
 	char name[100];
 	char description[100];
 	char department[100];
 	double price;
-	int quantity
+	int quantity;
 	int date;
 	int time;
+	int key;
 };
-struct product* hashArray[SIZE];
-int hashCode(int key) {
-	return key % SIZE;
+
+int hashCode(char * str, int size) {
+	int key;
+	int p = 1;
+	for (int i = strlen(str); i > 0 ; i--) {
+		key = (str[i] - 'a') * p;
+			p *= 10;
+	}
+	return key % size;
 }
-struct product* search(int key) {
+struct product* search(int key, int size, struct product** hashArray) {
 	//get the hash 
-	int hashIndex = hashCode(key);
+
+	int hashIndex = hashCode(key, size);
 
 	//move in array until an empty 
 	while (hashArray[hashIndex] != NULL) {
@@ -31,14 +39,19 @@ struct product* search(int key) {
 		++hashIndex;
 
 		//wrap around the table
-		hashIndex %= SIZE;
+		hashIndex %= size;
 	}
 
-	return NULL;
+	return hashArray;
 }
 
 
 int main(int argc, char* argv[])
 {
-
+	struct product hashArray[100];
+	FILE* fptr;
+	fptr = fopen(argv[1], "r");
+	char* temp;
+	fscanf(fptr, "%s", &temp);
+	printf("%s", temp);
 }
